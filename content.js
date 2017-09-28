42dddcf7087d8b92748e0056f2b014621a22bd5e
Joseph Paul Cohen <joseph@josephpcohen.com>
2017-09-28 17:04:49 -0400

    first draft

--- content.js
+++ content.js
@@ -1,22 +1,26 @@
 (function() {
   var location = window.location.href;
   var paper_id = location.substr(location.lastIndexOf('/') + 1);
-  $.ajax({
-    url: "http://www.shortscience.org/search?term=" + paper_id,
-    success: function(data) {
-      // This selector is brittle.
-      paper_page = $(data).find(".col-md-10 > div > a")[0].href.substr(location.lastIndexOf('/') + 1);
-      $.ajax({
-        url: "http://www.shortscience.org/" + paper_page,
-        success: function(data) {
-          // As is this one.
-          paper_summary = $(data).find(".source.panel-body.entry")[0]
-          // If there's a summary, open the page.
-          if (paper_summary.innerText !== "") {
-            window.open("http://www.shortscience.org/" + paper_page,'_blank');
-          }
-        }
-      })
-    }
-  });
+  //alert(paper_id);
+  
+  	// Create box and allow people to click and write summaries
+	a = document.createElement("div");
+	a.id = "shortscienceorg-box";
+	a.style.position="absolute";
+	a.style.top="0px";
+	a.style.right="0px";
+		
+	document.body.appendChild(a)
+	$("#shortscienceorg-box").html("<a href='http://www.shortscience.org/paper?bibtexKey=" + paper_id + "'>Write Summary</a>");
+	
+  
+	//read rss and show summaries that have already been written
+	
+	//	var xml = chrome.extension.getURL('rss-all.xml')
+	//	
+	//	$.get(xml, function(data) {
+	//		alert(jQuery.parseXML( data ))
+	//	});
+	//	
+	
 })();
